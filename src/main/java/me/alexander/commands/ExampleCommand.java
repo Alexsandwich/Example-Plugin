41d006f672f9bb0b0e5ac9f7d25cdf8faf4a950f
Alexander Milanovich <amilanov@vols.utk.edu>
2020-05-05 02:33:52 -0400

    Updated Info

--- src/main/java/me/alexander/commands/ExampleCommand.java
+++ src/main/java/me/alexander/commands/ExampleCommand.java
@@ -1,5 +1,6 @@
 package me.alexander.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,20 +13,32 @@ public class ExampleCommand implements CommandExecutor {
      *
      * @param commandSender the person who sent the command (console or player)
      * @param command the call when the command is ran
-     * @param s a string
+     * @param label a string
      * @param strings an array of strings
      * @return
      */
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        //Checking if the commandSender is a player
-        if(commandSender instanceof Player) {
-            //Defining what "player" will represent
-            Player player = (Player) commandSender;
 
-            //Sending a message upon successful command
-            player.sendMessage("Your command works!");
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] strings) {
+        // To Check if the command sender is a player
+        if (!(commandSender instanceof Player)) {
+            // Sending a message to the command sender
+            commandSender.sendMessage("You are not a player!");
+            return true;
+        } else {
+            // Defining what "player" will represent
+            Player player = (Player) commandSender;
+            // Checking if the player has a permission
+            if (player.hasPermission("plugin.permission")) {
+                // Send a message to the player if they have permission
+                player.sendMessage(ChatColor.GREEN + "You are a player and had permission to use this admin section.");
+                return true;
+            } else {
+                // Sending a message to the player if they don't have the permission
+                player.sendMessage(ChatColor.GREEN + "You are a default and were able to use this command.");
+                player.sendMessage("You do not have permission");
+                return true;
+            }
         }
-        // If the player (or console) runs the command correctly, we can return true
-        return true;
     }
 }
